cb331f5cdb0ee426b08235e309c26ed40c92c567
wchen342 <feiyu2817@gmail.com>
2019-07-24 01:50:38 -0400

    Remove dependency on Google Play GCM library

--- misc/android_studio_setup.sh
+++ misc/android_studio_setup.sh
@@ -14,21 +14,31 @@ pushd third_party
 ln -s ../../depot_tools
 popd
 
-# Need different GN flags than a release build
-mkdir -p out/Debug
-cat ../android_flags.debug.gn ../android_flags.gn > out/Debug/args.gn
-
-# Run gn first
-tools/gn/out/gn gen out/Debug --fail-on-unused-args
-
 ## Set compiler flags
 export AR=${AR:=llvm-ar}
 export NM=${NM:=llvm-nm}
 export CC=${CC:=clang}
 export CXX=${CXX:=clang++}
 
+# Need different GN flags than a release build
+output_folder=out/Debug_apk
+mkdir -p ${output_folder}
+cat ../android_flags.debug.gn ../android_flags.gn > ${output_folder}/args.gn
+
+# Run gn first
+tools/gn/out/gn gen ${output_folder} --fail-on-unused-args
+
 # Compile apk
-ninja -C out/Debug ${target}
+ninja -C ${output_folder} ${target}
+
+###
+# Develop folder
+output_folder=out/Debug
+mkdir -p ${output_folder}
+cat ../android_flags.debug.gn ../android_flags.gn > ${output_folder}/args.gn
+
+# Run gn first
+tools/gn/out/gn gen ${output_folder} --fail-on-unused-args
 
 # Generate gradle files
-python build/android/gradle/generate_gradle.py --target //chrome/android:${target} --output-directory out/Debug
+python build/android/gradle/generate_gradle.py --target //chrome/android:${target} --output-directory ${output_folder}
