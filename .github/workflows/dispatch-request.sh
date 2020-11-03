45540520df2f94ee54b9a25d1c697c576bd98977
wchen342 <feiyu2817@gmail.com>
2020-11-03 10:58:46 -0500

    Build dispatch test

---
+++ .github/workflows/dispatch-request.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+data='{'\
+'  "ref":"master",'\
+'  "inputs": {'\
+'    "github_ref":"'"${GITHUB_REF}"'",'\
+'    "github_sha":"'"${GITHUB_SHA}"'",'\
+'    "arch":"'"${ARCH}"'",'\
+'    "target":"'"${TARGET}"'"'\
+'  }'\
+'}'
+
+curl -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${PAT_TOKEN}" \
+  https://api.github.com/repos/"${REPO_USERNAME}"/"${REPO_NAME}"/actions/workflows/build.yml/dispatches \
+  -d "${data}"
